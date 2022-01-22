d15d307b3516a45cf7138bf9d14f45ca52ba33d2
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-01-22 23:51:48 +0400

    [Add] Footer 구현

---
+++ client/src/components/views/Footer.jsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+  return (
+    <div className="bg-hibye-100 h-80 text-hibye-10">
+      <div className="inner py-20 px-4">
+        <div className="flex justify-between mb-8">
+          <div className="text-2xl font-bold">SuSangYuHee</div>
+          <div className="text-sm">Copyright©SuSangYuHee</div>
+        </div>
+        <div className="flex justify-between">
+          <div className="text-sm">
+            <a href="https://github.com/strawberryoolongtea" className="block">
+              Subin kim
+            </a>
+            <a href="https://github.com/ParkSangBong" className="block">
+              Sangbong Park
+            </a>
+            <a href="https://github.com/YuchanJeong" className="block">
+              Yuchan Jeong
+            </a>
+            <a href="https://github.com/jenjenhub" className="block">
+              GangHeena Kim
+            </a>
+          </div>
+          <div className="self-end text-2xl font-bold">HiBye</div>
+        </div>
+      </div>
+    </div>
+  );
+}
