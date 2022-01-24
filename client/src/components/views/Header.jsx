1f32618d2f9cfcd7c3dcc2b92aea85d9c4231254
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-01-24 14:24:35 +0400

    [Add] Navbar 보드 목록 Get 및 로딩 구현
    데이터를 서버에서 받아오지 못했을 시 로딩 에니매이션 재생

--- client/src/components/views/Header.jsx
+++ client/src/components/views/Header.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useRef } from "react";
+import { useRef, useEffect, useCallback } from "react";
 import useDetectOutsideClick from "../../utils/useDetectOutsideClick";
-import { useCallback } from "react";
 import PrivateBoardCreateModal from "./PrivateBoardCreateModal";
+import { useSelector, useDispatch } from "react-redux";
+import { getPublicBoards } from "../../redux/modules/publicBoards";
+import { getPrivateBoards } from "../../redux/modules/privateBoards";
 
 export default function Header() {
-  // Todo: Sign in 기능과 커플 매칭 구현 후 변수에 할당
+  // Todo: 로그인, 유저정보 구현 후 적용 + sign out
   const isSignIn = true;
   const weMetOn = 1000;
+  const couple_id = 1;
 
   const navRef = useRef(null);
   const [isActive, setIsActive] = useDetectOutsideClick(navRef, false);
@@ -17,12 +20,25 @@ export default function Header() {
     setIsActive(!isActive);
   }, [isActive, setIsActive]);
 
+  const publicBoards = useSelector((state) => state.publicBoards);
+  const privateBoards = useSelector((state) => state.privateBoards);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPublicBoards());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (couple_id) {
+      dispatch(getPrivateBoards(couple_id));
+    }
+  }, [dispatch, couple_id]);
+
   return (
     <div className="bg-hibye-10 h-16">
       <div className="inner p-4">
         <GiHamburgerMenu className={`text-3xl absolute left-4 cursor-pointer ${isActive ? "text-hibye-60" : "text-hibye-80"}`} onClick={click} />
         <div ref={navRef} className="absolute top-16">
-          {isActive ? <Navbar click={click} /> : null}
+          {isActive ? <Navbar click={click} publicBoards={publicBoards} privateBoards={privateBoards} couple_id={couple_id} /> : null}
         </div>
         <Link to="/" className="text-hibye-80 text-3xl font-bold absolute left-1/2 -translate-x-2/4">
           HiBye
---
+++ client/src/redux/modules/publicBoards.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getPublicBoards = createAsyncThunk("boards/publicBoards", async () => {
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/board/public`);
+  return response.data;
+});
+
+const publicBoardsSlice = createSlice({
+  name: "publicBoardsSlice",
+  initialState: {
+    loading: true,
+    error: null,
+    data: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getPublicBoards.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getPublicBoards.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(getPublicBoards.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error;
+    });
+  },
+});
+
+export default publicBoardsSlice.reducer;
