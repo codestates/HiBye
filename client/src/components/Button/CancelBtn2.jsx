21b155045b101b7325f3aaae344b0d713a3fcd56
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-01-26 02:19:51 +0400

    [Add] ChatEditModal 제작 및 공통 버튼 추가 구현

---
+++ client/src/components/Button/CancelBtn2.jsx
@@ -0,0 +1,5 @@
+import { HiOutlineMinusCircle } from "react-icons/hi";
+
+export default function CancelBtn() {
+  return <HiOutlineMinusCircle className="text-2xl text-hibye-80 hover:text-hibye-10 hover:bg-hibye-80 rounded-full cursor-pointer duration-300" />;
+}
