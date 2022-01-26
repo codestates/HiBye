adcf7560e3bc41d749c6715941388c94bf25f59f
strawberryoolongtea <strawberrycream@kakao.com>
2022-01-27 08:29:25 +0900

    [Update] 탑버튼 클릭시 최상단으로 이동 기능 및 일정 스크롤 이상 나타나는 효과 구현

--- client/src/components/Button/TopBtn.jsx
+++ client/src/components/Button/TopBtn.jsx
@@ -1,4 +1,7 @@
 import { BsArrowUp } from "react-icons/bs";
 export default function TopBtn() {
-  return <BsArrowUp className="fixed bottom-10 right-40 z-50 text-5xl text-hibye-80 border-2 border-solid border-hibye-80 p-2 rounded-full" />;
+  const onClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  return <BsArrowUp onClick={onClick} className="fixed bottom-10 right-40 z-50 text-5xl text-hibye-80 border-2 border-solid border-hibye-80 p-2 rounded-full" />;
 }
