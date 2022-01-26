ccb3d474e3c723536b2ea16b37ac3a15d497f0d2
strawberryoolongtea <strawberrycream@kakao.com>
2022-01-27 08:35:24 +0900

    [Modify] 탑버튼 관련 변수명 수정

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -5,41 +5,29 @@ import BoardCards from "../components/views/BoardCards";
 
 function Main() {
   const [ScrollY, setScrollY] = useState(0);
-  const [btnStatus, setBtnStatus] = useState(false); // 버튼 상태
+  const [isOverScrollY, setIsOverScrollY] = useState(false);
 
-  const handleFollow = () => {
+  const handleShowButton = () => {
     setScrollY(window.pageYOffset);
     if (ScrollY > 100) {
-      // 100 이상이면 버튼이 보이게
-      setBtnStatus(true);
+      setIsOverScrollY(true);
     } else {
-      // 100 이하면 버튼이 사라지게
-      setBtnStatus(false);
+      setIsOverScrollY(false);
     }
-  };
-
-  const handleTop = () => {
-    // 클릭하면 스크롤이 위로 올라가는 함수
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-    setScrollY(0); // ScrollY 의 값을 초기화
-    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
   };
 
   useEffect(() => {
     const watch = () => {
-      window.addEventListener("scroll", handleFollow);
+      window.addEventListener("scroll", handleShowButton);
     };
     watch();
     return () => {
-      window.removeEventListener("scroll", handleFollow);
+      window.removeEventListener("scroll", handleShowButton);
     };
   });
   return (
     <div className="bg-hibye-10">
-      {btnStatus ? <TopBtn /> : null}
+      {isOverScrollY ? <TopBtn /> : null}
       {/* <TopBtn /> */}
       {/* banner */}
       <section className="w-screen h-fit bg-gradient-to-r from-hibye-60 to-hibye-80 pt-12">
