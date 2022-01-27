d788cfb40a6746ed85fab16a7ba0fd02abe93f55
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-01-28 02:03:13 +0400

    [Fix] 공통 부분 Route 밖으로 뺌

--- client/src/components/PostBoardMain.jsx
+++ client/src/components/PostBoardMain.jsx
@@ -1,19 +1,15 @@
 import dayjs from "dayjs";
 import SearchBtn from "./Button/SearchBtn";
-import EditBtn from "./Button/EditBtn";
-import CancelBtn, { CancelBtnSmall } from "./Button/CancelBtn";
+import { CancelBtnSmall } from "./Button/CancelBtn";
 import { BiArrowToLeft, BiLeftArrowAlt, BiArrowToRight, BiRightArrowAlt } from "react-icons/bi";
-import PrivateBoardEditModal from "./PrivateBoardEditModal";
 import swal from "sweetalert2";
 import axios from "axios";
 import { useEffect, useState, useCallback, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { openModal } from "../redux/modules/privateBoardEditModal";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useQuery } from "../utils/useQuery";
 
 export default function PostPage({ board, boardId }) {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const query = useQuery();
 
@@ -22,50 +18,6 @@ export default function PostPage({ board, boardId }) {
 
   // 유저 아이디 받아옴
   const user_id = useSelector((state) => state.user.id);
-
-  // 보드 수정 모달 오픈
-  const open = () => {
-    dispatch(openModal());
-  };
-
-  // 보드 삭제
-  const deleteBoard = () => {
-    swal
-      .fire({
-        title: "Are yue sure?",
-        showCancelButton: true,
-        icon: "warning",
-        confirmButtonColor: "#D70569",
-        confirmButtonText: "Yes, delete",
-        cancelButtonText: "Cancel",
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          axios
-            .delete(`${process.env.REACT_APP_API_URL}/board/${board.id}`)
-            .then(() => {
-              swal
-                .fire({
-                  title: "Board delete success",
-                  text: `"${board.name}" has been deleted`,
-                  icon: "success",
-                  confirmButtonColor: "#D70569",
-                })
-                .then(() => {
-                  window.location.reload();
-                });
-            })
-            .catch((err) => {
-              swal.fire({
-                title: "Board delete failed",
-                text: `${err}`,
-                icon: "error",
-                confirmButtonColor: "#D70569",
-              });
-            });
-        }
-      });
-  };
 
   // 검색
   const refSearch = useRef(null);
@@ -182,126 +134,103 @@ export default function PostPage({ board, boardId }) {
   };
 
   return (
-    <div className="bg-hibye-10">
-      <PrivateBoardEditModal board_id={board.id} prev_name={board.name} prev_desc={board.desc} />
-      <div className="inner pt-4">
-        <div className="mt-10 p-5">
-          <div className="mb-4 flex">
-            <div className="text-hibye-80 font-bold text-xl">{board.name}</div>
-            {board.couple_id ? (
-              <div className="flex">
-                <div className="ml-4 self-center" onClick={open}>
-                  <EditBtn />
-                </div>
-                <div className="ml-1 self-center" onClick={deleteBoard}>
-                  <CancelBtn />
-                </div>
-              </div>
-            ) : null}
+    <div>
+      <div className="border border-solid border-hibye-80 rounded-xl">
+        <div className="flex text-hibye-10 bg-hibye-60 p-2 justify-between rounded-t-xl">
+          <div className="flex">
+            <div className="w-16 mr-8 ml-2 text-center">
+              <div>No</div>
+            </div>
+            <div className="w-640px">
+              <div>Title</div>
+            </div>
           </div>
-          <div>
-            <div className="text-gray-80 text-base">{board.desc}</div>
+          <div className="flex">
+            <div className="w-44 text-center">
+              <div>Author</div>
+            </div>
+            <div className="w-32 text-center mr-4 ml-4">
+              <div>Date</div>
+            </div>
           </div>
         </div>
         <div>
-          <div className="border border-solid border-hibye-80 rounded-xl">
-            <div className="flex text-hibye-10 bg-hibye-60 p-2 justify-between rounded-t-xl">
-              <div className="flex">
-                <div className="w-16 mr-8 ml-2 text-center">
-                  <div>No</div>
-                </div>
-                <div className="w-640px">
-                  <div>Title</div>
-                </div>
-              </div>
-              <div className="flex">
-                <div className="w-44 text-center">
-                  <div>Author</div>
-                </div>
-                <div className="w-32 text-center mr-4 ml-4">
-                  <div>Date</div>
-                </div>
-              </div>
-            </div>
-            <div>
-              {posts.length === 0 ? (
-                !search ? (
-                  <div className="p-6 text-center text-lg text-gray-80 m-4">There's no post yet. Let's start posting!</div>
-                ) : (
-                  <div className="p-6 text-center text-lg text-gray-80 m-4">We couldn't find a post containing "{search}"</div>
-                )
-              ) : (
-                posts.map((post) => (
-                  <Link to={`/post/${boardId}/${post.id}`} key={post.id}>
-                    <div className="flex text-hibye-80 p-2 justify-between text-sm border-t border-hibye-80">
-                      <div className="flex">
-                        <div className="w-16 mr-8 ml-2 text-center">
-                          <div className="truncate">{post.rowNum}</div>
-                        </div>
-                        <div className="w-640px">
-                          <div className="truncate">{post.title}</div>
-                        </div>
-                      </div>
-                      <div className="flex">
-                        <div className="w-44 text-center">
-                          <div className="truncate">{post.User.username}</div>
-                        </div>
-                        <div className="w-32 text-center mr-4 ml-4">
-                          <div className="truncate">{dayjs(post.updatedAt).format("YYYY-MM-DD HH:MM")}</div>
-                        </div>
-                      </div>
+          {posts.length === 0 ? (
+            !search ? (
+              <div className="p-6 text-center text-lg text-gray-80 m-4">There's no post yet. Let's start posting!</div>
+            ) : (
+              <div className="p-6 text-center text-lg text-gray-80 m-4">We couldn't find a post containing "{search}"</div>
+            )
+          ) : (
+            posts.map((post) => (
+              <Link to={`/post/${boardId}/${post.id}`} key={post.id}>
+                <div className="flex text-hibye-80 p-2 justify-between text-sm border-t border-hibye-80">
+                  <div className="flex">
+                    <div className="w-16 mr-8 ml-2 text-center">
+                      <div className="truncate">{post.rowNum}</div>
                     </div>
-                  </Link>
-                ))
-              )}
+                    <div className="w-640px">
+                      <div className="truncate">{post.title}</div>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="w-44 text-center">
+                      <div className="truncate">{post.User.username}</div>
+                    </div>
+                    <div className="w-32 text-center mr-4 ml-4">
+                      <div className="truncate">{dayjs(post.updatedAt).format("YYYY-MM-DD HH:MM")}</div>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))
+          )}
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between mt-6 gap- items-center">
+          <div className="relative">
+            <input
+              type="text"
+              ref={refSearch}
+              onChange={onChangeSearch}
+              onKeyPress={onKeyPressSearch}
+              value={inputSearch}
+              className="absolute w-6 pl-2 text-transparent pr-2 bg-transparent duration-300 focus:bg-hibye-20 focus:w-40 focus:border-hibye-80 focus:text-hibye-100"
+            />
+            <div className="flex">
+              <SearchBtn className="absolute" />
+              {search ? (
+                <div className="flex items-center gap-1">
+                  <div className="ml-2 text-hibye-80">{querySearch}</div>
+                  <div onClick={searchCancel}>
+                    <CancelBtnSmall className="self-center" />
+                  </div>
+                </div>
+              ) : null}
             </div>
           </div>
+          {user_id ? <div className="button--pink">Post</div> : null}
+        </div>
+        <div className="flex justify-center mt-6 pb-40 gap-4">
+          <BiArrowToLeft className={`${btnStyle} ${page === 1 || page === 0 ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("first")} />
+          <BiLeftArrowAlt className={`${btnStyle} ${page === 1 || page === 0 ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("prev")} />
           <div>
-            <div className="flex justify-between mt-6 gap- items-center">
-              <div className="relative">
-                <input
-                  type="text"
-                  ref={refSearch}
-                  onChange={onChangeSearch}
-                  onKeyPress={onKeyPressSearch}
-                  value={inputSearch}
-                  className="absolute w-6 pl-2 text-transparent pr-2 bg-transparent duration-300 focus:bg-hibye-20 focus:w-40 focus:border-hibye-80 focus:text-hibye-100"
-                />
-                <div className="flex">
-                  <SearchBtn className="absolute" />
-                  {search ? (
-                    <div className="flex items-center gap-1">
-                      <div className="ml-2 text-hibye-80">{querySearch}</div>
-                      <div onClick={searchCancel}>
-                        <CancelBtnSmall className="self-center" />
-                      </div>
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-              {user_id ? <div className="button--pink">Post</div> : null}
-            </div>
-            <div className="flex justify-center mt-6 pb-40 gap-4">
-              <BiArrowToLeft className={`${btnStyle} ${page === 1 || page === 0 ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("first")} />
-              <BiLeftArrowAlt className={`${btnStyle} ${page === 1 || page === 0 ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("prev")} />
-              <div>
-                <input
-                  style={{ appearance: "none" }}
-                  type="number"
-                  onKeyUp={onKeyPress}
-                  min="1"
-                  max={lastPage}
-                  className="mr-1 bg-gray-10 text-center w-12 rounded-lg pl-1 pr-1"
-                  placeholder={queryPage}
-                  value={inputNum}
-                  onChange={onChangeNum}
-                />
-                {lastPage >= Number.MAX_SAFE_INTEGER ? null : <span>/ {lastPage}</span>}
-              </div>
-              <BiRightArrowAlt className={`${btnStyle} ${page === lastPage ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("next")} />
-              <BiArrowToRight className={`${btnStyle} ${page === lastPage ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("last")} />
-            </div>
+            <input
+              style={{ appearance: "none" }}
+              type="number"
+              onKeyUp={onKeyPress}
+              min="1"
+              max={lastPage}
+              className="mr-1 bg-gray-10 text-center w-12 rounded-lg pl-1 pr-1"
+              placeholder={queryPage}
+              value={inputNum}
+              onChange={onChangeNum}
+            />
+            {lastPage >= Number.MAX_SAFE_INTEGER ? null : <span>/ {lastPage}</span>}
           </div>
+          <BiRightArrowAlt className={`${btnStyle} ${page === lastPage ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("next")} />
+          <BiArrowToRight className={`${btnStyle} ${page === lastPage ? "text-hibye-40 cursor-default" : ""}`} onClick={() => goPage("last")} />
         </div>
       </div>
     </div>
--- client/src/pages/PostBoard.jsx
+++ client/src/pages/PostBoard.jsx
@@ -5,8 +5,17 @@ import useBoardCheck from "../utils/useBoardCheck";
 import Spinner from "../components/Spinner";
 import PostBoardMain from "../components/PostBoardMain";
 import Post from "../components/Post";
+import { openModal } from "../redux/modules/privateBoardEditModal";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import swal from "sweetalert2";
+import PrivateBoardEditModal from "../components/PrivateBoardEditModal";
+import EditBtn from "../components/Button/EditBtn";
+import CancelBtn from "../components/Button/CheckBtn";
 
 export default function PostBoard() {
+  const dispatch = useDispatch();
+
   // Params로 경로 확인
   const { boardId } = useParams();
 
@@ -20,6 +29,50 @@ export default function PostBoard() {
       setCheckTwoSec(true);
     }, 2000);
   });
+
+  // 보드 수정 모달 오픈
+  const open = () => {
+    dispatch(openModal());
+  };
+
+  // 보드 삭제
+  const deleteBoard = () => {
+    swal
+      .fire({
+        title: "Are yue sure?",
+        showCancelButton: true,
+        icon: "warning",
+        confirmButtonColor: "#D70569",
+        confirmButtonText: "Yes, delete",
+        cancelButtonText: "Cancel",
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .delete(`${process.env.REACT_APP_API_URL}/board/${board.id}`)
+            .then(() => {
+              swal
+                .fire({
+                  title: "Board delete success",
+                  text: `"${board.name}" has been deleted`,
+                  icon: "success",
+                  confirmButtonColor: "#D70569",
+                })
+                .then(() => {
+                  window.location.reload();
+                });
+            })
+            .catch((err) => {
+              swal.fire({
+                title: "Board delete failed",
+                text: `${err}`,
+                icon: "error",
+                confirmButtonColor: "#D70569",
+              });
+            });
+        }
+      });
+  };
 
   return (
     <>
@@ -36,10 +89,33 @@ export default function PostBoard() {
           </div>
         )
       ) : (
-        <Routes>
-          <Route exact path="/" element={<PostBoardMain board={board} boardId={boardId} />} />
-          <Route exact path="/:postId" element={<Post />} />
-        </Routes>
+        <div className="bg-hibye-10">
+          <PrivateBoardEditModal board_id={board.id} prev_name={board.name} prev_desc={board.desc} />
+          <div className="inner pt-4">
+            <div className="mt-10 p-5">
+              <div className="mb-4 flex">
+                <div className="text-hibye-80 font-bold text-xl">{board.name}</div>
+                {board.couple_id ? (
+                  <div className="flex">
+                    <div className="ml-4 self-center" onClick={open}>
+                      <EditBtn />
+                    </div>
+                    <div className="ml-1 self-center" onClick={deleteBoard}>
+                      <CancelBtn />
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+              <div>
+                <div className="text-gray-80 text-base">{board.desc}</div>
+              </div>
+            </div>
+            <Routes>
+              <Route exact path="/" element={<PostBoardMain board={board} boardId={boardId} />} />
+              <Route exact path="/:postId" element={<Post />} />
+            </Routes>
+          </div>
+        </div>
       )}
     </>
   );
