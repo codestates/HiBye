4ffedd8e67d3baf629b45633dfea318131bfae55
sangbongPark <meca231@gmail.com>
2022-01-27 17:22:05 +0900

    [Update] 페이징, 인덱싱, 정렬 구현,  변수 수정, 오타 수정

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const models = require("../models");
-require('dotenv').config();
+require("dotenv").config();
 
 router.get("/:board_id", async function (req, res) {
   const boardId = req.params.board_id;
   const todoList = await models.Todo.findAll({
-    where : {
-      board_id: boardId
-    }
-  })
+    where: {
+      board_id: boardId,
+    },
+    order: [["is_completed", "ASC"]],
+  });
   res.status(200).json({
     data: todoList,
-    message: "ok"
-  })
-})
+    message: "ok",
+  });
+});
 
 module.exports = router;
